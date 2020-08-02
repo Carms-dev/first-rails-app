36dcf39f69be9315ef37f43a76640ba69e5084ab
Carms Ng <carmenhyng89@gmail.com>
2020-08-02 13:17:23 -0400

    nav & footer components hook up, added form on contact pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
     def home
-        
+
     end
 
     def about
@@ -8,7 +8,11 @@ class PagesController < ApplicationController
     end
     
     def contact
+        @members = ['ohana', 'pumba', 'cynti', 'carms', 'iyan', 'shanny']
         
+        if params[:member]
+            @members = @members.select { |member| member.start_with?(params[:member].downcase) }
+        end
     end
     
 end
